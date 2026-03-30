12033406484a8c47ba3909022fc0cdc57c868d7d
Sam Matia <ssamat0020@gmail.com>
2026-03-30 23:03:42 +0100

    Add solution for Valid Palindrome II (trial 1)

--- leetcode/valid-palindrome-ii_trial1.py
+++ leetcode/valid-palindrome-ii_trial1.py
@@ -1,3 +1,22 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        def test_palindrome(l,r) : 
+            while l<r : 
+                if s[l] != s[r] :
+                    return False
+                l+=1
+                r-=1
+            return True
+
+        left, right = 0, len(s)-1
+        while left<right : 
+            if s[left] != s[right] : 
+                return test_palindrome(left+1, right) or test_palindrome(left, right-1)
+            
+            left += 1
+            right -= 1
+
+        return True
+        
+
         
