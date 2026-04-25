c8a29d2236a45fcd0dd4c655628e0e0004cb1ba8
Sam Matia <ssamat0020@gmail.com>
2026-04-25 17:05:21 +0100

    Add solution for Sqrt(x) (trial 1)

---
+++ leetcode/sqrt-x-_trial1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        if x<2 : 
+            return x
+
+        l,r = 1, x//2
+        output = 0
+
+        while l<=r : 
+            m = (l+r)//2
+
+            if m*m == x : 
+                return m
+            
+            elif m*m < x :
+                output = m
+                l = m+1
+
+            else : 
+                r = m-1
+
+        return output 
+
+
+        
